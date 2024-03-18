42aa86ed911acc6f889408ca3c5050b51ba01885
AstaryM <crimsonian84@gmail.com>
2024-03-18 11:14:00 +0200

    updated test version

--- main.py
+++ main.py
@@ -4,6 +4,25 @@ import Decoder
 from utils import *
 import sys
 
+def encode(image_path):
+    encoded_file_name = os.path.splitext(image_path)[0] + '.ast'
+    image = cv2.imread(image_path, cv2.IMREAD_COLOR)
+    old_size = os.path.getsize("test_image2.bmp")
+    new_size = os.path.getsize("temp")
+    print(f"compression process ended, new file size is {np.float16(100 * new_size / old_size)}% of the old file size")
+    cv2.imwrite()
+
+
+def decode(image_path):
+    decoded_file_name = os.path.splitext(image_path)[0]+'.tsa'
+    image = cv2.imread(image_path, cv2.IMREAD_COLOR)
+    decoded_image = Decoder.decoder(image_path)
+    cv2.imwrite(decoded_image)
+    #cv2.imshow("temp", result_image)
+    #cv2.waitKey(0)
+    #cv2.destroyAllWindows()
+
+
 if __name__ == '__main__':
     n = len(sys.argv)
     if len(sys.argv) != 3:
@@ -17,20 +36,3 @@ if __name__ == '__main__':
         print("error! invalid command")
 
 
-
-def encode(image_path):
-    encoded_file_name = os.path.splitext(image_path)[0] + '.ast'
-    image = cv2.imread(image_path, cv2.IMREAD_COLOR)
-    old_size = os.path.getsize("test_image2.bmp")
-    new_size = os.path.getsize("temp")
-    print(f"compression process ended, new file size is {np.float16(100 * new_size / old_size)}% of the old file size")
-    cv2.imwrite()
-
-def decode(image_path):
-    decoded_file_name = os.path.splitext(image_path)[0]+'.tsa'
-    image = cv2.imread(image_path, cv2.IMREAD_COLOR)
-    decoded_image = Decoder.decoder(image_path)
-    cv2.imwrite(decoded_image)
-    #cv2.imshow("temp", result_image)
-    #cv2.waitKey(0)
-    #cv2.destroyAllWindows()
